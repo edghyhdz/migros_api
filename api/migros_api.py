f8293a58d9a1e9b6f0d5aefa63becc067dae1b0e
edghyhdz <edgar.hdz@hotmail.com>
2020-09-19 09:14:12 +0200

    Raise errors when authentication failed

--- api/migros_api.py
+++ api/migros_api.py
@@ -5,6 +5,7 @@ import json
 import re
 import logging
 import os
+import sys
 
 
 FILE_PATH_CONF = "./"
@@ -22,6 +23,8 @@ logging.basicConfig(
 
 
 # TODO: 
+# FIND OTHER VALUE THAT TELLS YOU WHETHER YOU LOGGED IN SUCCESFULLY OR NOT
+
 # Add methods to search for all kasenbons
 # Fetch specific kasenbon with id
 # Get it into pdf format? 
@@ -51,71 +54,95 @@ class MigrosApi(object):
     def authenticate(self):
         """authenticate with init credentials
         """
-        self.headers = {
-            'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-        }
 
-        logging.info("Getting CSRF token")
-        response = self.session.get(self.login_url, headers=self.headers)
+        try: 
+            self.headers = {
+                'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+            }
 
-        # Build up cookies
-        self.headers['cookie'] = '; '.join([x.name + '=' + x.value for x in response.cookies])
-        self.headers['content-type'] = 'application/x-www-form-urlencoded'
+            logging.info("Getting CSRF token")
+            response = self.session.get(self.login_url, headers=self.headers)
 
+            # Build up cookies
+            self.headers['cookie'] = '; '.join([x.name + '=' + x.value for x in response.cookies])
+            self.headers['content-type'] = 'application/x-www-form-urlencoded'
+
+            
+            csrf = re.search(self.csfr_pattern, response.text).group(0)
+            csrf = eval(csrf)
+            display_token = csrf[0:5] + "...."
+            logging.info("Found CSR token: %s", display_token)
+            raw_data = "_csrf={0}&username={1}&password={2}".format(csrf, self.username, self.password)
+            
+            # Authenticate
+            response = self.session.post(self.login_url, headers=self.headers, data=raw_data)
+            response.raise_for_status()
+            status_code = response.status_code
+
+            logging.info("Response: %s", status_code)
+
+            matches = re.findall(r'{}'.format(self.username), response.text)
+            if matches:
+                logging.info("Logged in succesfully: %s", matches)
+            else:
+                raise ExceptionMigrosApi(1)
+
+        except ExceptionMigrosApi as err:
+            error_line = sys.exc_info()[-1].tb_lineno
+            logging.error("%s, error line: %s", *(err.error_codes[err.code], error_line))
         
-        csrf = re.search(self.csfr_pattern, response.text).group(0)
-        csrf = eval(csrf)
-        display_token = csrf[0:5] + "...."
-        logging.info("Found csr token: %s", display_token)
-        raw_data = "_csrf={0}&username={1}&password={2}".format(csrf, self.username, self.password)
-        
-        # Authenticate
-        response = self.session.post(self.login_url, headers=self.headers, data=raw_data)
-        response.raise_for_status()
-        status_code = response.status_code
+        except Exception as err:
+            error_line = sys.exc_info()[-1].tb_lineno
+            logging.error("Error when authenticating: %s, line: %s", *(err, error_line))
 
-        logging.info("Response: %s", status_code)
-
-        matches = re.findall(r'{}'.format(self.username), response.text)
-        if matches:
-            logging.info("Logged in succesfully")
 
     def log_cumulus(self):
         """log into cumulus
         """
 
-        self.authenticate()
+        try: 
+            self.authenticate()
 
-        # Update cookies
-        self.headers['cookie'] = '; '.join([x.name + '=' + x.value for x in self.session.cookies])
+            # Update cookies
+            self.headers['cookie'] = '; '.join([x.name + '=' + x.value for x in self.session.cookies])
 
-        # Update headers
-        self.headers.update(
-            {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-language": "en-US,en;q=0.9",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "same-origin",
-                "upgrade-insecure-requests": "1",
+            # Update headers
+            self.headers.update(
+                {
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "accept-language": "en-US,en;q=0.9",
+                    "sec-fetch-dest": "document",
+                    "sec-fetch-mode": "navigate",
+                    "sec-fetch-site": "same-origin",
+                    "upgrade-insecure-requests": "1",
+                }
+            )
+
+            params = {
+                "referrer": "https://www.migros.ch/resources/loginPage~lang=de~.html",
+                "referrerPolicy": "no-referrer-when-downgrade",
             }
-        )
 
-        params = {
-            "referrer": "https://www.migros.ch/resources/loginPage~lang=de~.html",
-            "referrerPolicy": "no-referrer-when-downgrade",
-        }
+            logging.info("Login into cumulus account")
+            response = self.session.get(self.cumulus_login, headers=self.headers, params=params)
+            status_code = response.status_code
+            logging.info("Status code: %s", status_code)
 
-        logging.info("Login into cumulus account")
-        response = self.session.get(self.cumulus_login, headers=self.headers, params=params)
-        status_code = response.status_code
-        logging.info("Status code: %s", status_code)
-
-        # TODO: finish all exceptions and so, in case log in is not succesfull
-        matches = re.findall(r'{}'.format("Edgar"), response.text)
-        if matches:
-            logging.info("Logged in succesfully to cumulus")
+            # TODO: finish all exceptions and so, in case log in is not succesfull
+            matches = re.findall(r'{}'.format("test"), response.text)
+            if matches:
+                logging.info("Logged in succesfully to cumulus")
+            else:
+                raise ExceptionMigrosApi(2)
+        
+        except ExceptionMigrosApi as err:
+            error_line = sys.exc_info()[-1].tb_lineno
+            logging.error("%s, error line: %s", *(err.error_codes[err.msg], error_line))
+        
+        except Exception as err:
+            error_line = sys.exc_info()[-1].tb_lineno
+            logging.error("Error when authenticating: %s, line: %s", *(err, error_line))
 
 
 class ExceptionMigrosApi(Exception):
@@ -124,7 +151,7 @@ class ExceptionMigrosApi(Exception):
     """
     error_codes = {
         '1': "Could not authenticate",
-        '2': ""
+        '2': "Could not authenticate to cumulus"
     }
 
     def __init__(self, code):
@@ -133,12 +160,13 @@ class ExceptionMigrosApi(Exception):
 
 
 
-
-
 if __name__ == "__main__":
     from getpass import getpass
-    PWD = os.environ.get("PASSWORD_MIGROS")
-    USERNAME = os.environ.get("USERNAME_MIGROS")
+    # PWD = os.environ.get("PASSWORD_MIGROS")
+    # USERNAME = os.environ.get("USERNAME_MIGROS")
+    PWD = None
+    USERNAME = None
+
     if not PWD:
         PWD = getpass("GIVE ME YOUR PSWORD: ")
         os.environ['PASSWORD_MIGROS'] = PWD
